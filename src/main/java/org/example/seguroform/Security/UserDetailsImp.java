9b203ae3858bd4b58d0cd69d7f4c761818db000b
JosuePi12 <josuepinedaquesada12@gmail.com>
2025-03-18 11:22:41 -0600

    clases de seguridad

---
+++ src/main/java/org/example/seguroform/Security/UserDetailsImp.java
@@ -0,0 +1,8 @@
+package org.example.seguroform.Security;
+
+import org.example.seguroform.logic.Usuario;
+
+public class UserDetailsImp {   //implements UserDetails
+    private Usuario usuario;
+
+}
